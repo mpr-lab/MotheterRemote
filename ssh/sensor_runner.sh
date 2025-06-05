dd36871b2decad1f448e2e050e0629abaaf10923
Skye W Worster <131273379+SWorster@users.noreply.github.com>
2025-06-05 11:22:40 -0400

    grep debugging

--- ssh/sensor_runner.sh
+++ ssh/sensor_runner.sh
@@ -5,9 +5,13 @@ dt="$(date '+%d/%m/%Y %H:%M:%S');"
 
 echo "Running sensor_runner.sh"
 
-processes=$(ps -ef | grep [s]ensor_stream.py)
-if [[ $? == 1 ]]; then # grep found nothing
-    echo "Sensor streaming program not running!"
+all_procs=$(ps -ef)
+num_inst=$(echo "$all_procs" | grep [s]ensor_stream | wc -l)
+
+if test $num_inst == 1; then # grep found one thing (the actual program)
+    echo "Already running sensor_streaming.py"
+elif test $num_inst == 0; then # grep returned something
+    echo "Sensor streaming program not running! Attempting to start now."
 
     # redirect stdout to log file
     echo $dt >> /var/tmp/ssh/sensor_output.txt
@@ -19,11 +23,6 @@ if [[ $? == 1 ]]; then # grep found nothing
 
     # run python program in background (don't wait for it to finish, just let shell die)
     /usr/bin/python3 ~/MotheterRemote/ssh/sensor_stream.py 2>> /var/tmp/ssh_debug/sensor_error.txt 1>>/var/tmp/ssh/sensor_output.txt &
-
-elif [[ $processes ]]; then # grep returned something
-    : # do nothing
-elif [[ $? != 0 ]]; then # error
-    echo "Command failed (not grep)."
 else # something else went wrong
-    echo "Command failed."
+    echo "Command failed for unknown reasons; manual debugging required."
 fi
