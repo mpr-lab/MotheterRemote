01c1db5944a98092c75416e4b14ea126802b6699
Skye W Worster <131273379+SWorster@users.noreply.github.com>
2025-06-05 11:06:08 -0400

    typo

--- ssh/sensor_stream.py
+++ ssh/sensor_stream.py
@@ -9,15 +9,16 @@ sensor: sensor_ssh.SQM
 
 def main():
     global sensor
-    if configs_ssh.device_type == "SQMLU":
+    if configs_ssh.device_type == "SQM-LU":
         sensor = sensor_ssh.SQMLU()
-    elif configs_ssh.device_type == "SQMLE":
+    elif configs_ssh.device_type == "SQM-LE":
         sensor = sensor_ssh.SQMLE()
     else:
         print(
             "Configs don't list device as sensor; sensor_stream.py shouldn't run.",
             file=sys.stderr,
         )
+        return
 
     print(
         "sensor_stream.py running. Will monitor sensor over serial until killed. Output is directed to stdout (should be appended to a log file via bash script).",
