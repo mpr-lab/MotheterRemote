1c4ef158f1d9e08f70004abfd1c3682a11aa3ab6
buddy <kluong@smith.edu>
2025-06-11 13:42:59 -0400

    ssh instructions

--- GUI/SetupWizard.java
+++ GUI/SetupWizard.java
@@ -37,6 +37,7 @@ public class SetupWizard extends JFrame {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(600, 500);
         setLocationRelativeTo(null);
+        autoDetectSystem();
 
         // Sections
         sectionStart.put("Host Setup", 0);
@@ -54,7 +55,6 @@ public class SetupWizard extends JFrame {
         wizardSteps.add(buildSSH_Step4());          // 8
         wizardSteps.add(buildFinalPanel());         // 9
 
-        autoDetectSystem();
 
         // Add all to cardPanel
         for (int i = 0; i < wizardSteps.size(); i++) {
@@ -252,6 +252,9 @@ public class SetupWizard extends JFrame {
         JPanel inner = new JPanel();
         inner.setLayout(new BoxLayout(inner, BoxLayout.Y_AXIS));
 
+        JPanel bottom = new JPanel();
+        bottom.setLayout(new BoxLayout(bottom, BoxLayout.Y_AXIS));
+
         JTextArea info = util.buildTextArea(panel, 200);
         info.setText("""
                 Tailscale is a VPN service that essentially creates a virtual LAN. Devices that are logged in on a network are given IP addresses and can be accessed by any other networked device
@@ -260,11 +263,13 @@ public class SetupWizard extends JFrame {
         JLabel question = new JLabel("Will you be using Tailscale?");
 
         inner.add(info);
-        inner.add(question);
-        inner.add(yTailscale);
+        bottom.add(question);
+        bottom.add(Box.createRigidArea(new Dimension(0, 10)));
+        bottom.add(yTailscale);
 
         panel.add(title, BorderLayout.NORTH);
         panel.add(inner, BorderLayout.CENTER);
+        panel.add(bottom, BorderLayout.SOUTH);
         return panel;
     }
 
@@ -297,6 +302,9 @@ public class SetupWizard extends JFrame {
         JPanel inner = new JPanel();
         inner.setLayout(new BoxLayout(inner, BoxLayout.Y_AXIS));
 
+        JPanel bottom = new JPanel();
+        bottom.setLayout(new BoxLayout(bottom, BoxLayout.Y_AXIS));
+
         JTextArea info = util.buildTextArea(panel, 300);
         info.setText("""
                 Tailscale is a VPN service that essentially creates a virtual LAN. Devices that are logged in on a network are given IP addresses and can be accessed by any other networked device
@@ -306,12 +314,13 @@ public class SetupWizard extends JFrame {
 
         inner.add(info);
         inner.add(Box.createRigidArea(new Dimension(0, 10)));
-        inner.add(question);
-        inner.add(Box.createRigidArea(new Dimension(0, 10)));
-        inner.add(yRadio);
+        bottom.add(question);
+        bottom.add(Box.createRigidArea(new Dimension(0, 10)));
+        bottom.add(yRadio);
 
         panel.add(title, BorderLayout.NORTH);
         panel.add(inner, BorderLayout.CENTER);
+        panel.add(bottom, BorderLayout.SOUTH);
         return panel;
     }
 
@@ -335,6 +344,8 @@ public class SetupWizard extends JFrame {
         profilesPanel = new JPanel();
         profilesPanel.setLayout(new BoxLayout(profilesPanel, BoxLayout.Y_AXIS));
         panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
+        panel.add(new JLabel("Configure Raspberry Pi Profiles"), BorderLayout.NORTH);
 
 
         JButton addProfile = new JButton("Add RPi Profile");
@@ -387,6 +398,7 @@ public class SetupWizard extends JFrame {
 
         JPanel inner = new JPanel();
         inner.setLayout(new BoxLayout(inner, BoxLayout.Y_AXIS));
+        inner.add(Box.createRigidArea(new Dimension(0, 10)));
 
         JTextArea description = util.buildTextArea(inner, 30);
         description.setText("This GUI uses Secure Shell to remotely access and run commands on the raspberry pi. This section of the setup will walk you through setting up Secure Shell (SSH), and using it to connect to a raspberry pi.");
@@ -416,34 +428,33 @@ public class SetupWizard extends JFrame {
         // STEP 1: CHECK IS SSH INSTALLED< IF IT's NOT, INSTALL IT
         JPanel step1 = new JPanel();
         step1.setLayout(new BoxLayout(step1, BoxLayout.Y_AXIS));
-        step1.add(new JLabel("Step 1: Check is SSH is installed"));
+        step1.add(new JLabel("Step 1: Check if SSH is installed"));
         step1.add(Box.createRigidArea(new Dimension(0, 10)));
 
-        // TODO: INSTRUCTIONS BASED ON OPERATING SYSTEM
         JTextArea copyI1 = util.buildTextArea(step1, 30);
-        copyI1.setText("Open up a new terminal. First, check whether or not you have ssh installed. This looks different on different operating systems.");
-
-//        // Windows
-//        String sshCmd = "systemctl status sshd";
-//        JPanel checkSSHRow = buildCopyRow(sshCmd);
-
-//        // Mac
-//        String sshCmd = "sudo systemsetup -getremotelogin";
-//        JPanel checkSSHRow = buildCopyRow(sshCmd);
-
-        // Linux
-        String sshCmd = "systemctl status sshd";
+        copyI1.setText("Open up a new terminal. First, check whether or not you have ssh installed. This looks different on different operating systems. Your operating system is "+". If that does not seem right....");
+        String sshCmd = "";
+        JTextArea proceedI1 = util.buildTextArea(step1, 45);
+        switch (detectedOS) {
+            case "windows" -> {
+                sshCmd = "ssh";
+            }
+            case "mac"     -> {
+                sshCmd = "sudo systemsetup -getremotelogin";
+            }
+            case "linux"   -> {
+                sshCmd = "systemctl status sshd";
+                proceedI1.setText("For linux users, there should be a line printed in the output of the terminal after running the first command that says 'Active: active (running)' if SSH is active. If this is the case, then move onto [STEP 2]. If not, move on to [STEP 2a].");
+            }
+        }
         JPanel checkSSHRow = buildCopyRow(sshCmd);
-
-        JTextArea proceedI1 = util.buildTextArea(step1, 30);
-        proceedI1.setText("If there is a file called id_ed_25519, then you already have an SSH key. Proceed to [STEP 2]. If you do not have a file called id_ed_25519, proceed to [STEP 2a].");
 
         step1.add(copyI1);
         step1.add(Box.createRigidArea(new Dimension(0, 10)));
         step1.add(checkSSHRow);
-        step1.add(Box.createRigidArea(new Dimension(0, 20)));
-        step1.add(proceedI1);
         step1.add(Box.createRigidArea(new Dimension(0, 10)));
+        step1.add(proceedI1);
+        step1.add(Box.createRigidArea(new Dimension(0, 30)));
 
         // STEP 1a: DOWNLOAD SSH IF NOT DOWNLOADED
         JPanel step1a = new JPanel();
@@ -451,16 +462,72 @@ public class SetupWizard extends JFrame {
         step1a.add(new JLabel("Step 1a: Download SSH"));
         step1a.add(Box.createRigidArea(new Dimension(0, 10)));
 
-        JTextArea copyI1a = util.buildTextArea(step1, 30);
-        copyI1.setText("If you do not already have ssh installed: ");
+//        JTextArea copyI1a = util.buildTextArea(step1, 15);
+//        copyI1.setText("If you do not already have ssh installed: ");
+//        step1a.add(copyI1a);
+//        step1a.add(Box.createRigidArea(new Dimension(0, 10)));
 
-        String downloadCmd = "ssh";
-        JPanel downloadSSHRow = buildCopyRow(downloadCmd);
+        switch (detectedOS) {
+            case "windows" -> {
+                JTextArea download = util.buildTextArea(step1, 200);
+                download.setText("""
+                        If SSH is installed, it will display help information about the command, including its usage and available options. If the terminal returns: "SSH not recognized", then this means SSH is not installed or enabled on your system. Follow these steps to download ssh:
+                                  
+                        1) Go to Settings > Apps > Apps & Features > Optional Features
+                                            
+                        2) Click on "Add a feature" and select OpenSSH Client
+                                            
+                        3) Click "Install" to add the OpenSSH Client to your system
+                                            
+                        4) You may need to restart your system for the changes to take effect
+                                            
+                        5) After installing OpenSSH Client, open a new command prompt or PowerShell and type `ssh` again to confirm that it is now recognized.                
+                        """);
+                step1a.add(download);
+                step1a.add(Box.createRigidArea(new Dimension(0, 10)));
+            }
+            case "mac"     -> {
+                JTextArea download = util.buildTextArea(step1, 200);
+                download.setText("""
+                Mac operating systems should come pre-installed with SSH. If SSH is enabled, the output will show "Remote Login: On". If it's off, it will show "Remote Login: Off". SSH may be disabled on your system. In that case, follow these steps to enable it:
+                
+                1) Open System Preferences (Apple menu > System Preferences)
+                
+                2) Go to Sharing
+                
+                3) Look for "Remote Login" on the left-hand side
+                
+                4) To enable SSH, activate the checkmark next to "Remote Login"
+                """);
+                step1a.add(download);
+                step1a.add(Box.createRigidArea(new Dimension(0, 10)));
+            }
+            case "linux"   -> {
+                JTextArea download = util.buildTextArea(step1, 60);
+                download.setText("""
+                If the terminal says anything along the lines of "Unit file sshd.service does not exist", then you do not have ssh installed. Run the following command in your terminal to install SSH:
+                """);
+                step1a.add(download);
+                step1a.add(Box.createRigidArea(new Dimension(0, 10)));
 
-        step1a.add(copyI1a);
-        step1a.add(Box.createRigidArea(new Dimension(0, 10)));
-        step1a.add(downloadSSHRow);
-        step1a.add(Box.createRigidArea(new Dimension(0, 20)));
+                String downloadCmd = "sudo apt install openssh-server openssh-client";
+                JPanel downloadSSHRow = buildCopyRow(downloadCmd);
+                step1a.add(downloadSSHRow);
+                step1a.add(Box.createRigidArea(new Dimension(0, 10)));
+
+                JTextArea enable = util.buildTextArea(step1, 45);
+                enable.setText("""
+                It may be the case that SSH is installed but is not active. To enable SSH, run the following command in your terminal:
+                """);
+                step1a.add(download);
+                step1a.add(Box.createRigidArea(new Dimension(0, 10)));
+
+                String enableCmd = "sudo systemctl start ssh";
+                JPanel enableSSHRow = buildCopyRow(enableCmd);
+                step1a.add(enableSSHRow);
+                step1a.add(Box.createRigidArea(new Dimension(0, 20)));
+            }
+        }
 
         inner.add(step1);
         inner.add(step1a);
@@ -497,14 +564,14 @@ public class SetupWizard extends JFrame {
         step2.add(copyI2);
         step2.add(Box.createRigidArea(new Dimension(0, 10)));
         step2.add(checkKeySSHRow);
-        step2.add(Box.createRigidArea(new Dimension(0, 20)));
-        step2.add(proceedI2);
         step2.add(Box.createRigidArea(new Dimension(0, 10)));
+        step2.add(proceedI2);
+        step2.add(Box.createRigidArea(new Dimension(0, 30)));
 
         // STEP 2 GENERATE SSH KEY
         JPanel step2a = new JPanel();
         step2a.setLayout(new BoxLayout(step2a, BoxLayout.Y_AXIS));
-        step2a.add(new JLabel("Step 2: Generate a SSH Key"));
+        step2a.add(new JLabel("Step 2a: Generate a SSH Key"));
         step2a.add(Box.createRigidArea(new Dimension(0, 10)));
 
         JTextArea copyI2a = util.buildTextArea(step2, 30);
@@ -516,7 +583,7 @@ public class SetupWizard extends JFrame {
         step2a.add(copyI2a);
         step2a.add(Box.createRigidArea(new Dimension(0, 10)));
         step2a.add(genSSHRow);
-        step2a.add(Box.createRigidArea(new Dimension(0, 20)));
+        step2a.add(Box.createRigidArea(new Dimension(0, 10)));
 
         // Add to Panel
         inner.add(step2);
@@ -615,6 +682,7 @@ public class SetupWizard extends JFrame {
         panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 
         JLabel label = new JLabel("Setup complete.");
+        util.setFullWidth.accept(label);
         JButton finish = new JButton("Finish");
         finish.addActionListener(e -> saveProfilesAndExit());
         panel.add(label);
